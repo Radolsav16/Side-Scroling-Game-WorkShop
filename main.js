e54514e9e7f7751154ec4969f9f966b1ff4ecd01
Radoslav <todorovradoslav6@gmail.com>
2024-10-22 23:54:17 +0300

    Make  fireballs functionality

--- main.js
+++ main.js
@@ -1,19 +1,35 @@
 // Gets Elements 
 const gameStartDiv = document.querySelector('.game-start');
 const gameScoreDiv = document.querySelector('.game-score');
+const gamePoints = document.querySelector('.points');
 const gameAreaDiv = document.querySelector('.game-area');
 const gameOverDiv = document.querySelector('.game-over');
+
+const coordinatesMax = {
+    top:15,
+    bottom:540,
+    left:15,
+    rigth:1000
+}
 
 let keys = {};
 let player = {
     x:150,
     y:100,
     width:0,
-    heigth:0
+    heigth:0,
+    lastTimeFireBall:0
 };
+
+let scene = {
+    score:0
+}
+
 let game = {
     speed:2,
-    movingMultiplayer:4
+    movingMultiplayer:4,
+    fireMultiplyer:5,
+    fireInterval : 200
 };
 
 gameStartDiv.addEventListener('click',onGameStart);
@@ -43,40 +59,96 @@ document.addEventListener('keyup',onKeyUp);
 
 function onKeyDown(e){
     keys[e.code] = true;
-    console.log(keys)
 }
 
 function onKeyUp(e){
     keys[e.code] = false;
-    console.log(keys)
 }
 
-function gameAction(){
-    const wizard = document.querySelector('.wizard');
-    console.log(player)
+function addFireBall(player){
+   
+    let fireball = document.createElement('div');
     
-    if(keys.ArrowUp && player.y + player.heigth < gameAreaDiv.offsetHeight){ 
+    fireball.classList.add('fire-ball');
+
+    fireball.style.top = (player.y + player.heigth / 3 - 5) + 'px';
+    fireball.x = player.x + player.width;
+    fireball.style.left = fireball.x + 'px';
+
+
+
+    gameAreaDiv.appendChild(fireball);
+ }
+
+function gameAction(timestamp){
+    const wizard = document.querySelector('.wizard');
+    
+
+   
+
+
+    scene.score++;
+
+
+    if(keys.Space && timestamp - player.lastTimeFireBall > game.fireInterval){
+        wizard.classList.add('wizard-fire');
+        addFireBall(player)
+        player.lastTimeFireBall = timestamp
+    }else{
+        wizard.classList.remove('wizard-fire');
+
+    }
+    
+    
+    let isinAir = (player.y + player.heigth) <= gameAreaDiv.offsetHeight;
+
+    if(isinAir){
+        player.y += game.speed;
+    }
+
+    
+    
+    if(keys.ArrowUp &&  player.y > coordinatesMax.top){ 
         player.y -= game.speed * game.movingMultiplayer
     }
 
-    if(keys.ArrowDown){
+    if(keys.ArrowDown && player.y < coordinatesMax.bottom){
         player.y += game.speed * game.movingMultiplayer;
     }
 
-    if(keys.ArrowLeft && player.x + player.width < gameAreaDiv.offsetWidth){
+    if(keys.ArrowLeft && player.x > coordinatesMax.left){
         player.x -= game.speed * game.movingMultiplayer;
 
     }
 
-    if(keys.ArrowRight){
+    if(keys.ArrowRight && player.x  < coordinatesMax.rigth){
         player.x += game.speed * game.movingMultiplayer
 
     }
+
+    const fireballs = document.querySelectorAll('.fire-ball');
+
+   
+    
+
+    fireballs.forEach((fireball)=>{
+        fireball.x += game.speed * game.fireMultiplyer;
+        fireball.style.left = fireball.x + 'px';
+
+        if(fireball.x + fireball.offsetWidth > gameAreaDiv.offsetWidth){
+            fireball.parentElement.removeChild(fireball)
+        }
+    })
 
 
     wizard.style.top = `${player.y}px`;
     wizard.style.left = `${player.x}px`;
 
+   
+
+
+
+    gameScoreDiv.textContent = `${scene.score} pts.`;
 
 
 
